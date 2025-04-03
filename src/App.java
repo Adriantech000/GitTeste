f5793032ba7aeb8126083e7f534f83e0d6e8b21f
Adriantech000 <felipe.gamer.barroso@gmail.com>
2025-04-03 14:14:09 -0300

    programa de altura do chico e zé feitos

--- src/App.java
+++ src/App.java
@@ -2,9 +2,24 @@
 public class App {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Estou no git papai!");
-        int lindo=0;
-        System.out.println(lindo);
+        double alturaChico = 1.50;
+        double alturaZe = 1.10;
+
+        double altPorAnoC = 0.02;
+        double altPorAnoZ = 0.03;
+        int anos = 0;
+
+        while (alturaZe < alturaChico) {
+            alturaChico += altPorAnoC;
+            alturaZe += altPorAnoZ;
+            anos++;
+            
+            String alturaCFormatada = String.format("%.2f", alturaChico);
+            String alturaZFormatada = String.format("%.2f", alturaZe);
+
+            System.out.println("chico: "+alturaCFormatada);
+            System.out.println("zé: "+alturaZFormatada);
+        }
+        System.out.println("Zé alcançou em: " + anos + " anos");
     }
 }
