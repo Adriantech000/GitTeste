d6bb5fbb672d7343c60241e2af8a10078a377ae1
Adriantech000 <felipe.gamer.barroso@gmail.com>
2025-04-03 13:31:11 -0300

    adição da variavel lindo

--- src/App.java
+++ src/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Estou no git papai!");
-
+        int lindo=0;
+        System.out.println(lindo);
     }
 }
